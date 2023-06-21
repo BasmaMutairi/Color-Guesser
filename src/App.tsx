d59de718ad9cd5963bceec77942e17e53654d489
Parea <BasmaMutairi@gmail.com>
2023-06-21 21:45:16 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import React from 'react';
+import { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { get } from 'http';
+
+
+const getRandomColor =() => {
+  const digits= ['0', '1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+
+  const color= new Array(6)
+  .fill("")
+  .map(() => digits[Math.floor(Math.random() * digits.length)])
+  .join("");
+
+  return `#${color}`;
+
+  console.log(color);
+  return color;
+}
+
+enum Result {
+  Correct,
+  Wrong,
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [color,setColor] = useState("");
+  const [answers, setAnswers] = useState<string[]>([]);
+  const[result,setResult] = useState< Result | undefined> (undefined);
+
+  const generateColors = () => {
+  {/* to generate a random color */}
+      const actualColor =getRandomColor();
+      setColor(actualColor);
+      setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(
+        () => 0.5 - Math.random()
+        )
+      );
+      };
+
+  useEffect(() => {
+    generateColors();
+  },[]);
+
+
+
+
+  function handleAnswerClicked(answer: string){
+    if (answer === color) {
+      setResult(Result.Correct);
+      generateColors();
+    }else{
+      setResult(Result.Wrong);
+    }
+
+  }
+
+
+ return <div className="App">
+  <div>
+  {/* to create a box */}
+  <div className="guess-me" style={{background: color}}></div>
+
+    {answers.map ((answer) => (
+      <button onClick={() => handleAnswerClicked(answer)} key={answer}>
+        {answer}
+      </button>
+    ))}
+    {result === Result.Correct && <div className="correct">You Got It!</div>}
+    {result === Result.Wrong && <div className="wrong">Wrong Answer!</div>}
+
+  </div>
+ </div>;
 }
 
 export default App;
